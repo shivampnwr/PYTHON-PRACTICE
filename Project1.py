ff19c354bb85d220b1b5fc7e373f39f8d54cdda6
shivampnwr <gujjarshivam318@gmail.com>
2026-05-05 23:52:30 +0530

    efficent version of the problem

--- Project1.py
+++ Project1.py
@@ -1,25 +1,20 @@
 # Data Cleaner (list procdessing)
 data =[10,None,20,10,"",30,None,40]
-# Remove None and empty string from the list
-junk_data = [None, ""]
-unique_data=[]
-
-cleaned_data = []
-for item in data:
-    if item not in junk_data:
-        cleaned_data.append(item)
-print(cleaned_data)
-for item in cleaned_data:
-    if item not in unique_data:
-        unique_data.append(item)
+# remove duplicate values
+unique_data = list(set(data))
 print(unique_data)
-
-
-
-original_length = len(data)
+# remove  invaled values
+unique_data.remove(None)
+unique_data.remove("")
+print(unique_data)
+# count of removed item
+original_length=len(data)
 cleaned_length = len(unique_data)
 removed_length = original_length - cleaned_length
 print(removed_length)
 # sorted list
-unique_data.sort()
-print(unique_data)
+print(sorted(unique_data))
+
+
+
+
