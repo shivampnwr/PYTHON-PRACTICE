746e651f1e3f23a338674e0b117bae2461dd6cc5
shivampnwr <gujjarshivam318@gmail.com>
2026-05-06 23:06:22 +0530

    Day 3

--- PROJECT 3/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-key=input(" Enter the name to search contact number :\n")
-# value = phonebook.get(key)
-# print(value)
-# # delete contact 
-# x = input("Enter the key to delete the contact number :\n")
-# phonebook.pop(x)
-# print(phonebook)
